886395cc9c7d5e1eb3cf4debd32f14e3f469024d
Esther Kang <estherkang14@gmail.com>
2020-05-20 18:51:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index do |element|
+  array.each_with_index do { |element, index|
     p element
   end
 end
